9d088bfc5839a82ba5790e8338dbb09c57c26a19
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-07-03 14:21:45 -0700

    ..

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,6 @@
 //.. Require ..//
 let inquirer = require('inquirer');
 let mysql = require('mysql');
-let itemPick = process.argv[0];
-let amountPurchase = process.argv[0];
 //... Create a connection to the  SQL database ..//
 let connection = mysql.createConnection({
     host: 'localhost',
@@ -40,7 +38,7 @@ function displayInventory(){
 };
 
 //..This function will prompt the user to choose an item and purchase quantity
-function askUser()/*A*/{
+function askUser() {
     inquirer.prompt([{
         type: 'input',
         name: 'item_id',
@@ -55,12 +53,58 @@ function askUser()/*A*/{
     },
     {
         type: 'input',
-        name: 'quantity',
+        name: 'amount',
         message: 'How many are you purchasing?',
         filter: Number   
     }
-]).then
-/*A*/};
+]).then(function(answer) {
+    
+    let itemPicked = input.item_id;
+    let amount = input.amount;
+    let query = 'SELECT * FROM products WHERE = ?';
+    connection.query(query, {item_id: answer.product_name}, function(error,response){
+        if (error) throw error;
+        let available = response[0].in_stock;
+        let cost = response[0].price;
+        let pick = response[0].item_id;
+        if (available >= answer.amount) {
+            completePurchase(in_stock,price,answer.item_id, answer.amount);
+        } else console.log('Sorry, we are out of stock!');
+    });
+    let completePurchase = function(in_stock, item_id,){
+        let updateInventory = in_stock - amount;
+        let purchasePrice = price * amount;
+        let query = 'UPDATE products SET ? WHERE ?';
+        connection.query(query, [{
+            in_stock: updateInventory,
+            item_id: itemPicked,
+        }], function(error, response) {
+            if (error) throw error;
+            console.log('We are all set!');
+            console.log('You have me charged for the amount of' + purchasePrice);
+        });
+    };
+});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /* A. function askUser(){}; 
